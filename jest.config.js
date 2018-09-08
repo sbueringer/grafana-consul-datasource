b74750ca7a4b2d846803a076010d061c5b77a273
Stefan Bueringer <sbueringer@gmail.com>
2018-09-08 15:01:02 +0200

    fixed coverage tests

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,38 @@
 module.exports = {
-  verbose: true,
-  "globals": {
-    "ts-jest": {
-      "tsConfigFile": "tsconfig.jest.json",
-      "useBabelrc": true
-    }
-  },
-  "moduleNameMapper": {
-    // 'app/core/utils/datemath': '<rootDir>/node_modules/grafana-sdk-mocks/app/core/utils/datemath.ts',
-    // 'app/core/utils/kbn': '<rootDir>/src/__mocks__/kbn.ts',
-    'app/plugins/sdk': '<rootDir>/node_modules/grafana-sdk-mocks/app/plugins/sdk.ts',
-  },
-  "transformIgnorePatterns": [
-    "node_modules/(?!(grafana-sdk-mocks))"
-  ],
-  "transform": {
-    // "^.+\\.tsx?$": "ts-jest"
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  "testRegex": "(\\.|/)([jt]est)\\.ts$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json"
-  ]
+    verbose: true,
+    "globals": {
+        "ts-jest": {
+            "tsConfigFile": "tsconfig.jest.json",
+            "useBabelrc": true
+        }
+    },
+    "moduleNameMapper": {
+        // 'app/core/utils/datemath': '<rootDir>/node_modules/grafana-sdk-mocks/app/core/utils/datemath.ts',
+        // 'app/core/utils/kbn': '<rootDir>/src/__mocks__/kbn.ts',
+        'app/plugins/sdk': '<rootDir>/node_modules/grafana-sdk-mocks/app/plugins/sdk.ts',
+    },
+    "transformIgnorePatterns": [
+        "node_modules/(?!(grafana-sdk-mocks))"
+    ],
+    "transform": {
+        // "^.+\\.tsx?$": "ts-jest"
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    "testRegex": "(\\.|/)([jt]est)\\.ts$",
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json"
+    ],
+    "collectCoverageFrom": [
+        "src/*.ts",
+        "!**/node_modules/**",
+        "!**/vendor/**"
+    ],
+    "coverageDirectory": "<rootDir>/coverage",
+    "coverageReporters": [
+        "json",
+    ]
 };
